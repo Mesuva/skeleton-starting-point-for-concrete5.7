16199506438a6ee348684494a8c658a42b46bb60
Ryan Hewitt <ryan@mesuva.com.au>
2016-06-28 10:38:43 +0930

    Update page_theme.php

--- page_theme.php
+++ page_theme.php
@@ -2,13 +2,26 @@
 namespace Application\Theme\Skeleton;
 use Concrete\Core\Page\Theme\Theme;
 class PageTheme extends Theme {
-
-	public function registerAssets() {
-		$this->requireAsset('javascript', 'jquery');
-	}
-
+	
     protected $pThemeGridFrameworkHandle = 'SkeletonGrid';
 
+    public function registerAssets() {
+	$this->requireAsset('javascript', 'jquery');
+	// $this->requireAsset('css', 'font-awesome');
+	// $this->requireAsset('css', 'core/lightbox');
+	// $this->requireAsset('javascript', 'core/lightbox');
+    }
+
+    public function getThemeName()
+    {
+        return t('Skeleton Starting Point');
+    }
+
+    public function getThemeDescription()
+    {
+        return t('A concrete5 theme based on Skeleton');
+    }
+    
     public function getThemeBlockClasses()
     {
 
